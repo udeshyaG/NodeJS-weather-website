00ba75ee87f8b9ded60cfd1c267f010aa4a80d83
Udeshya <me@myself.com>
2020-06-02 11:14:56 +0530

    Prepare app before deploying to heroku

--- src/app.js
+++ src/app.js
@@ -90,4 +90,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
